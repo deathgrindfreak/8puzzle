313cf7b1593b8b824119a0d065221baf1f6b7cb6
Cooper Bell <john.cooper.bell@gmail.com>
2017-01-09 11:46:58 -0600

    Fix validation

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 (function() {
     'use strict'
 
-    var errorColor = "#a94442";
+    var errorColor = "red";
+    var cellColor = "#efefef";
+    var emptyColor = "#ababab";
 
-    function createTable(tableId, size) {
+    function createTable(tableId, size, defaults) {
         var tab = document.getElementById(tableId);
         // Clear the table
         tab.innerHTML = "";
+
+        // Create the cells and inputs
         for (var i = 0; i < size; i++) {
             var row = tab.insertRow(i);
             for (var j = 0; j < size; j++) {
@@ -20,26 +24,44 @@
                 inp.setAttribute("type", "text");
                 inp.setAttribute("maxlength", "1");
                 inp.setAttribute("required", true);
-
-                inp.oninput = function() {
-                    checkInput(id);
-                };
+                inp.style.background = cellColor;
+                inp.oninput = checkInput(id);
 
                 cell.appendChild(inp);
             }
         }
 
-        function checkInput(checkId) {
-            var checkInp = document.getElementById("grid-input-" + checkId);
-            checkInp.style.background = "none";
-
+        // Set the defaults and fire the oninput listener
+        if (defaults) {
             for (var id = 0; id < size * size; id++) {
                 var inp = document.getElementById("grid-input-" + id);
-                console.log("checkId:", checkId);
-                console.log("check:", checkInp.value, "inp-" + id, ":", inp.value);
-                if (id !== checkId && inp.value && inp.value === checkInp.value) {
-                    checkInp.style.background = errorColor;
-                    break;
+                inp.value = defaults[id];
+                inp.oninput();
+            }
+        }
+
+        // Returns handler for input changes
+        function checkInput(checkId) {
+            return function() {
+                var checkInp = document.getElementById("grid-input-" + checkId);
+
+                // Determine if duplicates are present
+                for (var id = 0; id < size * size; id++) {
+                    var inp = document.getElementById("grid-input-" + id);
+                    if (id !== checkId && inp.value && inp.value === checkInp.value) {
+                        checkInp.style.background = errorColor;
+                        return;
+                    }
+                }
+
+                // Set the color/value if no duplicate was detected
+                if (checkInp.value == "0") {
+                    checkInp.style.background = emptyColor;
+                    checkInp.value = "";
+                    checkInp.required = false;
+                } else {
+                    checkInp.style.background = cellColor;
+                    checkInp.required = true;
                 }
             }
         }
@@ -51,6 +73,34 @@
         createTable("user-table", sizeSelection.value);
     };
 
+    // Click for reset button
+    var resetButton = document.getElementById("reset-button");
+    resetButton.onclick = function(e) {
+        e.preventDefault();
+
+        var size = document.getElementById("grid-size").value;
+        for (var i = 0; i < size * size; i++) {
+            var inp = document.getElementById("grid-input-" + i);
+            inp.value = "";
+            inp.style.background = cellColor;
+        }
+    }
+    
+    // Click for solve button
+    var solveButton = document.getElementById("solve-button");
+    solveButton.onclick = function(e) {
+        var size = document.getElementById("grid-size").value;
+        console.log(getGridValues(size));
+
+        // Retrieve values from grid
+        function getGridValues(size) {
+            var vals = [];
+            for (var id = 0; id < size * size; id++)
+                vals.push(document.getElementById("grid-input-" + id).value);
+            return vals.map(function(m) { return m === "" ? 0 : parseInt(m); });
+        }
+    };
+
     // Initialize 
-    createTable("user-table", 3);
+    createTable("user-table", 3, [0, 1, 3, 4, 2, 5, 7, 8, 6]);
 })();
